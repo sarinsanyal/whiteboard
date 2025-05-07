65815bcac8badab7adfeaf81f3e108d77a964238
SagnikGos <sagnik.gos06@gmail.com>
2025-05-07 22:43:48 +0530

    ts-socket-errorFix

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,9 @@
 "use client";
 
-import io from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
-export const socket = io();
+const SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:3001";
+
+export const socket: Socket = io(SERVER_URL, {
+  transports: ['websocket', 'polling'],
+});
