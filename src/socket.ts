451827ed7c06cc24e0cfd4063b076e33893ab39a
sarinsanyal <sarinsanyal2005@gmail.com>
2025-05-29 12:07:45 +0530

    Added localhost dev functionality

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,9 @@
 // socket.ts
 "use client";
 import io from "socket.io-client";
-export const socket = io("https://whiteboard-socket.onrender.com");
+
+const SOCKET_URL = (process.env.NODE_ENV === "development")
+    ? "http://localhost:3001"
+    : "https://whiteboard-socket.onrender.com"
+
+export const socket = io(SOCKET_URL);
